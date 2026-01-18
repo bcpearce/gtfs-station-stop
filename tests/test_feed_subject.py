49c3e35f3512473f80af6a8835d31208beea5b7a
Benjamin Pearce <admin@bcpearce.com>
2026-01-18 09:36:51 -0500

    Add tests for rate limiting

--- tests/test_feed_subject.py
+++ tests/test_feed_subject.py
@@ -1,3 +1,5 @@
+import math
+
 from gtfs_station_stop.feed_subject import FeedSubject
 
 
@@ -13,3 +15,15 @@ def test_init_FeedSubject():
 def test_FeedSubject_update_does_not_throw_with_zero_uris():
     fs = FeedSubject([])
     fs.update()
+
+
+def test_rate_limiting():
+    fs = FeedSubject([])
+    fs.delay_between_api_calls = 0.5
+    assert math.isclose(fs.max_api_calls_per_second, 2, abs_tol=0.0001)
+
+    fs.max_api_calls_per_second = 3
+    assert math.isclose(fs.delay_between_api_calls, 0.33333, abs_tol=0.0001)
+
+    fs.max_api_calls_per_second = None
+    assert fs.delay_between_api_calls is None
